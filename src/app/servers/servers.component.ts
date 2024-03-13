bd0e232e07fe3c1b1f4e7cbd12bd10ee5b662c21
Mark Kinz <mark.kinz@tylertech.com>
2024-03-13 13:29:41 -0700

    Built up to Section 2, lecture 24

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  template: `<app-server></app-server>
+             <app-server></app-server>`,
+  styleUrl: './servers.component.css'
+})
+export class ServersComponent {
+
+}
